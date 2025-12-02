877a94eb84b85f5ca1fd76eed874dbde1a025333
Sumit Vajarinkar 🚀 <vajarinkarss@gmail.com>
2025-12-02 20:55:09 +0530

    Update className for contact section styling

--- components/contact.tsx
+++ components/contact.tsx
@@ -15,7 +15,7 @@ export default function Contact() {
     <motion.section
       id="contact"
       ref={ref}
-      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      className="scroll-mt-28 mb-28 sm:mb-40"
       initial={{
         opacity: 0,
       }}
