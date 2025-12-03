c7cccea0cb095744c2b4f5dab8305eca5a5821ca
Sumit Vajarinkar 🚀 <vajarinkarss@gmail.com>
2025-12-03 08:03:42 +0530

    Fix for social link alignment

--- components/intro.tsx
+++ components/intro.tsx
@@ -91,12 +91,6 @@ export default function Intro() {
           Contact me here{" "}
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
- <motion.div className="flex sm:flex-row"
-          initial={{ opacity: 0, y: 100 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{
-            delay: 0.1,
-          }}>
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href="https://drive.google.com/file/d/1pkJjDMGcB30kSJbVnibv7jMC4nm82xM4/view?usp=drivesdk"
@@ -105,7 +99,12 @@ export default function Intro() {
           Resume{" "}
           <GoMoveToEnd className="opacity-60 group-hover:translate-x-1 transition" />
         </a>
-
+ <motion.div className="flex sm:flex-row"
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.1,
+          }}>
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://www.linkedin.com/in/sumitvajarinkar/"
@@ -143,7 +142,7 @@ export default function Intro() {
           <SiGeeksforgeeks />
         </a>
       </motion.div>
-      </motion.div>
-    </section>
+    </motion.div>
+  </section>
   );
 }
